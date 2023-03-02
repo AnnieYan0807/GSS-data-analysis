c140a51bcaf2e0eeca46124028a78d4363f30e97
Aliyah Maxine Ramos <aliyahmaxine.ramos@mail.utoronto.ca>
2023-03-02 16:27:59 -0500

    First set of graphs made

--- scripts/03-rough_work.R
+++ scripts/03-rough_work.R
@@ -11,16 +11,63 @@
 
 library(tidyverse)
 
-sum_bully_data |> ggplot(aes(x = date, y = sum)) + 
+# Age VS. Internet Hours
+cleaned_2016gss_data |> ggplot(aes(x = age, y = wwwhr)) + 
   geom_col() +
   theme(axis.text.x = element_text(angle = 45,hjust=1), 
         axis.line = element_line(colour = "black"),
         title = element_text(size = 8)) +
   labs(
-    x = "Date",
-    y = "Search Frequency",
-    fill = "State Name"
+    x = "Age",
+    y = "Internet Hours"
   )
+
+# Income VS. Internet Hours
+cleaned_2016gss_data |> ggplot(aes(x = income, y = wwwhr)) + 
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 45,hjust=1), 
+        axis.line = element_line(colour = "black"),
+        title = element_text(size = 8)) +
+  labs(
+    x = "Income",
+    y = "Internet Hours"
+  )
+
+# Education VS. Internet Hours
+cleaned_2016gss_data |> ggplot(aes(x = educ, y = wwwhr)) + 
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 45,hjust=1), 
+        axis.line = element_line(colour = "black"),
+        title = element_text(size = 8)) +
+  labs(
+    x = "Education",
+    y = "Internet Hours"
+  )
+# REMINDER: find average THEN graph
+
+# Sex VS. Internet Hours
+cleaned_2016gss_data |> ggplot(aes(x = sex, y = wwwhr)) + 
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 45,hjust=1), 
+        axis.line = element_line(colour = "black"),
+        title = element_text(size = 8)) +
+  labs(
+    x = "Sex",
+    y = "Internet Hours"
+  )
+# REMINDER: find average THEN graph
+
+# Race VS. Internet Hours
+cleaned_2016gss_data |> ggplot(aes(x = race, y = wwwhr)) + 
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 45,hjust=1), 
+        axis.line = element_line(colour = "black"),
+        title = element_text(size = 8)) +
+  labs(
+    x = "Race",
+    y = "Internet Hours"
+  )
+# REMINDER: find average THEN graph
 
 #### Tables ####
 
