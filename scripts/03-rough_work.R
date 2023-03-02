ced5408581c141523de2099dbdc360837bca611e
Aliyah Maxine Ramos <aliyahmaxine.ramos@mail.utoronto.ca>
2023-03-02 15:11:50 -0500

    Adding rough work script

---
+++ scripts/03-rough_work.R
@@ -0,0 +1,8 @@
+#### Preamble ####
+# Purpose: Rough work.
+# Author: Sakura Ariga, Annie Yan, Aliyah Maxine Ramos
+# Data: 2  March 2023
+# Contact: aliyahmaxine.ramos@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: None.
+# Any other information needed? None
